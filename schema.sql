e3fd96a70afde6fc727f753b8fb75bc64e355a3c
Katri <katri.kuoppala@helsinki.fi>
2023-05-30 12:36:35 +0300

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE points (id SERIAL PRIMARY KEY, game_id INTEGER REFERENCES games, user_id INTEGER REFERENCES users, point INTEGER);
+
+CREATE TABLE bets (id SERIAL PRIMARY KEY, game_id INTEGER REFERENCES games, user_id INTEGER REFERENCES users, goals_home INTEGER, goals_visitor INTEGER);
+
+CREATE TABLE outcomes (id SERIAL PRIMARY KEY, game_id INTEGER REFERENCES games UNIQUE, goals_home INTEGER, goals_visitor INTEGER, scored INTEGER);
+
+CREATE TABLE games (id SERIAL PRIMARY KEY, home_team TEXT, visitor_team TEXT, date TIMESTAMP);
+
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin INTEGER);
+
