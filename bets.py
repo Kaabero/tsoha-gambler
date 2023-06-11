c60c37bf6e3a6e63975c359281e0d2638980a06a
Katri <katri.kuoppala@helsinki.fi>
2023-06-11 20:31:33 +0300

    Pylint fixes

--- bets.py
+++ bets.py
@@ -1,29 +1,38 @@
-from db import db
-from sqlalchemy.sql import text
 from datetime import datetime
+from sqlalchemy.sql import text
+from db import db
 
 def bet(game_id, user_id, goals_home, goals_visitor):
 
     sql = text("SELECT date FROM games WHERE id=:game_id")
-    date = db.session.execute(sql, {'game_id':game_id})
-    
+    date = db.session.execute(sql, {'game_id': game_id})
+
     for row in date:
         if row.date > datetime.now():
-        
+
             try:
-                sql = text("INSERT INTO bets (game_id, user_id, goals_home, goals_visitor) VALUES (:game_id, :user_id, :goals_home, :goals_visitor)")
-                db.session.execute(sql, {"game_id":game_id, "user_id":user_id, "goals_home":goals_home, "goals_visitor":goals_visitor})
+                sql = text(
+                    """INSERT INTO bets (game_id, user_id, goals_home, goals_visitor)
+                       VALUES (:game_id, :user_id, :goals_home, :goals_visitor)""")
+                db.session.execute(sql,
+                                   {"game_id": game_id,
+                                    "user_id": user_id,
+                                    "goals_home": goals_home,
+                                    "goals_visitor": goals_visitor})
                 db.session.commit()
                 return True
-            except:
+            except BaseException:
                 return False
-    
+
     return False
+
 
 def already_placed_bet(user_id, game_id):
 
-    sql = text("SELECT user_id, game_id FROM bets WHERE game_id=:game_id AND user_id=:user_id")
-    already_placed_bets = db.session.execute(sql, {"game_id":game_id, "user_id":user_id})
+    sql = text(
+        "SELECT user_id, game_id FROM bets WHERE game_id=:game_id AND user_id=:user_id")
+    already_placed_bets = db.session.execute(
+        sql, {"game_id": game_id, "user_id": user_id})
     data = []
     for row in already_placed_bets:
         data.append({
@@ -31,39 +40,46 @@ def already_placed_bet(user_id, game_id):
             'game_id': row.game_id
         })
 
-    if data:
-        return True
+    return data
 
 def get_bets():
-    sql = text("SELECT G.home_team, G.visitor_team, G.date, U.username, B.goals_home, B.goals_visitor FROM bets B, users U, games G WHERE B.game_id=G.id AND B.user_id = U.id AND G.date BETWEEN NOW() AND G.date ORDER BY G.date")
+    sql = text(
+        """SELECT G.home_team, G.visitor_team, G.date, U.username, B.goals_home, B.goals_visitor
+                  FROM bets B, users U, games G
+                  WHERE B.game_id=G.id AND B.user_id = U.id 
+                  AND G.date BETWEEN NOW() AND G.date ORDER BY G.date""")
     bets = db.session.execute(sql).fetchall()
     return bets
 
+
 def own_bets(user_id):
-    sql = text("SELECT B.game_id, G.home_team, G.visitor_team, G.date, B.goals_home, B.goals_visitor FROM bets B, games G WHERE G.id=B.game_id AND user_id=:user_id AND G.date BETWEEN NOW() AND G.date ORDER BY G.date")
-    bets = db.session.execute(sql, {"user_id":user_id}).fetchall()
-    print(bets)
+    sql = text(
+        """SELECT B.game_id, G.home_team, G.visitor_team, G.date, B.goals_home, B.goals_visitor
+                  FROM bets B, games G
+                  WHERE G.id=B.game_id AND user_id=:user_id AND G.date BETWEEN NOW() 
+                  AND G.date ORDER BY G.date""")
+    bets = db.session.execute(sql, {"user_id": user_id}).fetchall()
     return bets
+
 
 def delete_bet(user_id, game_id):
 
-    sql = text("SELECT date FROM games G, bets B WHERE B.game_id = G.id AND B.game_id=:game_id AND B.user_id=:user_id")
-    date = db.session.execute(sql, {"game_id":game_id, "user_id":user_id})
+    sql = text(
+        """SELECT date FROM games G, bets B
+        WHERE B.game_id = G.id AND B.game_id=:game_id AND B.user_id=:user_id""")
+    date = db.session.execute(sql, {"game_id": game_id, "user_id": user_id})
 
     for row in date:
         if row.date > datetime.now():
-            try:    
-                sql = text("DELETE FROM bets WHERE game_id=:game_id AND user_id=:user_id")
-                db.session.execute(sql, {"user_id":user_id, "game_id":game_id})
+            try:
+                sql = text(
+                    "DELETE FROM bets WHERE game_id=:game_id AND user_id=:user_id")
+                db.session.execute(
+                    sql, {"user_id": user_id, "game_id": game_id})
                 db.session.commit()
                 return True
-            except:
+            except BaseException:
                 return False
         else:
             return False
     return False
-
-
-
-       
-
--- games.py
+++ games.py
@@ -1,62 +1,82 @@
-from db import db
-from sqlalchemy.sql import text
 from datetime import datetime
+from sqlalchemy.sql import text
+from db import db
+
 
 def add_game(home_team, visitor_team, day, time):
 
     if home_team.lower() == visitor_team.lower():
         return False
-    
-    splitted_date=day.split("-")
-    splitted_time=time.split(":")
-    
-    date = datetime(int(splitted_date[0]), int(splitted_date[1]), int(splitted_date[2]), int(splitted_time[0]), int(splitted_time[1]))
+
+    splitted_date = day.split("-")
+    splitted_time = time.split(":")
+
+    date = datetime(int(splitted_date[0]), int(splitted_date[1]), int(
+        splitted_date[2]), int(splitted_time[0]), int(splitted_time[1]))
 
     if date < datetime.now():
         return False
-    else:
-        sql = text("INSERT INTO games (home_team, visitor_team, date) VALUES (:home_team, :visitor_team, :date)")
-        db.session.execute(sql, {"home_team":home_team, "visitor_team":visitor_team, "date":date})
-        db.session.commit()
-        return True
+
+    sql = text(
+        """INSERT INTO games (home_team, visitor_team, date) 
+           VALUES (:home_team, :visitor_team, :date)""")
+    db.session.execute(sql,
+                        {"home_team": home_team,
+                        "visitor_team": visitor_team,
+                        "date": date})
+    db.session.commit()
+    return True
+
 
 def add_outcome(game_id, goals_home, goals_visitor):
 
     sql = text("SELECT date FROM games WHERE id=:game_id")
-    date = db.session.execute(sql, {"game_id":game_id})
+    date = db.session.execute(sql, {"game_id": game_id})
 
     for row in date:
         if row.date < datetime.now():
-            try:    
-                sql = text("INSERT INTO outcomes (game_id, goals_home, goals_visitor, scored) VALUES (:game_id, :goals_home, :goals_visitor, :scored)")
-                db.session.execute(sql, {"game_id":game_id, "goals_home":goals_home, "goals_visitor":goals_visitor, "scored":0})
+            try:
+                sql = text(
+                    """INSERT INTO outcomes (game_id, goals_home, goals_visitor, scored) 
+                       VALUES (:game_id, :goals_home, :goals_visitor, :scored)""")
+                db.session.execute(sql,
+                                   {"game_id": game_id,
+                                    "goals_home": goals_home,
+                                    "goals_visitor": goals_visitor,
+                                    "scored": 0})
                 db.session.commit()
                 return True
-            except:
+            except BaseException:
                 return False
     return False
 
+
 def get_open_games():
-    sql = text("SELECT * FROM games WHERE date BETWEEN NOW() AND date ORDER BY date")
+    sql = text(
+        "SELECT * FROM games WHERE date BETWEEN NOW() AND date ORDER BY date")
     games = db.session.execute(sql).fetchall()
     db.session.commit()
     return games
+
 
 def get_closed_games():
-    sql = text("SELECT * FROM games WHERE date < NOW() AND outcome_added is null ORDER BY date")
+    sql = text(
+        "SELECT * FROM games WHERE date < NOW() AND outcome_added is null ORDER BY date")
     games = db.session.execute(sql).fetchall()
     db.session.commit()
     return games
 
+
 def get_outcome(game_id):
-    sql = text("SELECT goals_home, goals_visitor FROM outcomes WHERE game_id=:game_id")
-    goals = games = db.session.execute(sql, {"game_id":game_id}).fetchall()
+    sql = text(
+        "SELECT goals_home, goals_visitor FROM outcomes WHERE game_id=:game_id")
+    goals = db.session.execute(sql, {"game_id": game_id}).fetchall()
     return goals[0]
-    
+
 
 def mark_as_registered(game_id):
     sql = text("UPDATE games SET outcome_added=1 WHERE id=:game_id")
-    db.session.execute(sql, {"game_id":game_id})
+    db.session.execute(sql, {"game_id": game_id})
     db.session.commit()
 
 def delete_all():
@@ -69,14 +89,10 @@ def delete_all():
     sql = text("DELETE FROM games")
     db.session.execute(sql)
     db.session.commit()
-    return
 
 def get_outcomes():
-    sql = text("SELECT G.home_team, G.visitor_team, G.date, O.goals_home, O.goals_visitor FROM outcomes O, games G WHERE O.game_id=G.id ORDER BY G.date")
+    sql = text("""SELECT G.home_team, G.visitor_team, G.date, O.goals_home, O.goals_visitor
+                  FROM outcomes O, games G WHERE O.game_id=G.id ORDER BY G.date""")
     outcomes = db.session.execute(sql).fetchall()
     db.session.commit()
     return outcomes
-
-
-
-
